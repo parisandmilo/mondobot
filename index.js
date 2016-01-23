3ec92339cecf7b27c69001356869f57d811120e6
parisandmilo <di.klhy@gmail.com>
2016-01-23 13:02:19 +0000

    rename env vars to mondo_

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var mondo = require('mondo-bank');
 require('dotenv').config();
 
 mondo.token({
-    client_id: process.env.client_id,
-    client_secret: process.env.client_secret,
-    username: process.env.username,
-    password: process.env.password
+    client_id: process.env.mondo_client_id,
+    client_secret: process.env.mondo_client_secret,
+    username: process.env.mondo_username,
+    password: process.env.mondo_password
   }, 
   function(err, value){
     //value is the token here
