0862ff3f912c9788c855cafc093569d2a6a16011
HugoDF <hdifrancesco@gmail.com>
2016-01-23 17:02:28 +0000

    add rewrite.js

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ var controller = Botkit.slackbot({
     debug: true,
 });
 
+function formatGBP(value){
+  var str = value +'';
+  var len = str.length;
+  var text = str.slice(0, len - 2) + "." + str.slice(len - 2, len) + " GBP";
+  return text;
+}
+
 var bot = controller.spawn({
     token: process.env.slack_token
 }).startRTM();
@@ -36,39 +43,62 @@ var bot = controller.spawn({
 controller.on('bot_channel_join', function(bot, message){
     bot.say({
         channel: message.channel,
-        text: "Yo wassup"
+        text: "Hello Mondo!!!"
     });
 });
+
 controller.hears(['hello','hi'], 'direct_message,direct_mention,mention', function(bot, message){
   bot.reply(message, "hello");
 });
+//spambot
 controller.hears(['mondo'], 'ambient', function(bot, message){
   bot.reply(message, "Hey, I'm the MondoBot" + String.fromCharCode(169));
 });
 //this is where the API integration starts
+
+//accounts
 controller.hears(['account', 'accounts'], 'direct_message,direct_mention,mention', function(bot, message){
   bot.reply(message, "These are your accounts: ");
   mondo.accounts(mondoToken, function(err, value){
-    var i = 0;
-    var text = value.accounts.map(function(account){
-      i++;
-      return i + ". Account: " + account.id + ", " + account.description;
+    var text = value.accounts.map(function(account, index){
+      return index + 1 + ". Account: " + account.id + ", " + account.description;
     }).join("\n");
     bot.reply(message, text);
   });
 });
+
+//balance command
 controller.hears(['balance(.*)'], 'direct_message,direct_mention,mention', function(bot, message){
   bot.reply(message, "Getting balance", function(){
     mondo.accounts(mondoToken, function(err, value){
       if(value.accounts.length == 1){
         var account_id = value.accounts[0].id;
         var text = "Account: " + value.accounts[0].description + ", id: " + account_id;
-        console.log(text);
         bot.reply(message, text, function(){
           mondo.balance(account_id, mondoToken, function(err, value){
-            var balanceStr = value.balance +'';
-            var len = balanceStr.length;
-            var text = balanceStr.slice(0, len - 2) + "." + balanceStr.slice(len - 2, len) + " GBP";
+            var text = formatGBP(value.balance);
+            bot.reply(message, text);
+          });
+        });
+      }
+    });
+  });
+});
+
+//transactions command
+controller.hears(['transactions'], 'direct_message,direct_mention,mention', function(bot, message){
+  bot.reply(message, "Getting transactions", function(){
+    mondo.accounts(mondoToken, function(err, value){
+      if(value.accounts.length == 1){
+        var account_id = value.accounts[0].id;
+        var text = "Account: " + value.accounts[0].description + ", id: " + account_id;
+        bot.reply(message, text, function(){
+          mondo.transactions(account_id, mondoToken, function(err, value){
+
+            var text = JSON.stringify(value.transactions);
+            var text = value.transactions.map(function(transaction, index){
+              return index + 1 + ". transaction: " + transaction.description + ", " + transaction.description + ", " + formatGBP(transaction.amount);
+            }).join("\n");
             bot.reply(message, text);
           });
         });
---
+++ rewrite.js
@@ -0,0 +1,12 @@
+// var os = require('os');
+// var fs = require('fs');
+// var lineReader = require('readline').createInterface({
+//   input: fs.createReadStream('./node_modules/mondo-bank/lib/api.values.json')
+// });
+// lineReader.on('line', function (line) {
+//   console.log('Line from file:', line);
+//   if(line.includes("host")){
+//     //change
+//   }
+//   return line;
+// });
