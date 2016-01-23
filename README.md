251e3642045820752507ecd03f3b2ab5ad5cbfbe
HugoDF <hdifrancesco@gmail.com>
2016-01-23 14:14:03 +0000

    make the bot answer with whoever's token is in .env

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Building a mondo slack integrating running botkit, and mondo-bank
 `npm install` to install dependencies
 
 `cp .env.example .env` and change the environment variables to your creds
+
+#Coding style
+2 space tabs, tabs translated to spaces
+
+We're using mongo-bank with callback syntax
--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 var mondo = require('mondo-bank');
 require('dotenv').config();
 
-mondo.token({
-    client_id: process.env.mondo_client_id,
-    client_secret: process.env.mondo_client_secret,
-    username: process.env.mondo_username,
-    password: process.env.mondo_password
-  }, 
-  function(err, value){
-    //value is the token here
-    //Do some stuff :)
-  }
-);
+// mondo.token({
+//     client_id: process.env.mondo_client_id,
+//     client_secret: process.env.mondo_client_secret,
+//     username: process.env.mondo_username,
+//     password: process.env.mondo_password
+//   }, 
+//   function(err, value){
+//     //value is the token here
+//     //Do some stuff :)
+//   }
+// );
+var mondoToken = process.env.mondo_token;
+
+if (!process.env.slacktoken) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.slacktoken
+}).startRTM();
+
+controller.on('bot_channel_join', function(bot, message){
+    bot.say({
+        channel: message.channel,
+        text: "Yo wassup"
+    });
+});
+
+controller.hears(['account', 'accounts'], 'direct_message,direct_mention,mention', function(bot, message){
+  mondo.accounts(mondoToken, function(err, value){
+    var text = JSON.stringify(value);
+    bot.reply(message, text);
+  });
+});
