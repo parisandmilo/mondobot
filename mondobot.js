99a59a20cd39f09917c489c73be7e63934e1b3b3
HugoDF <hdifrancesco@gmail.com>
2016-01-24 15:56:57 +0000

    do the thing to work on deployed :o (maybe)

--- mondobot.js
+++ mondobot.js
@@ -208,9 +208,109 @@ controller.hears('.*', 'direct_message, direct_mention', function (bot, message)
     });
 
   var wit = witbot.process(message.text, bot, message);
-  wit.hears("transaction", 0.5, require('./lib/replies/transaction.js'));
-  wit.hears("account", 0.5, require('./lib/replies/account.js'))
-  wit.hears("balance", 0.5, require('./lib/replies/balance.js'));
+  wit.hears("transaction", 0.5, function(bot, message, outcome){
+    bot.botkit.log("transaction");
+    console.log(bot);
+    bot.api.reactions.add({
+      timestamp: message.ts,
+      channel: message.channel,
+      name: 'thinking_face',
+    },function(err,res) {
+      if (err) {
+        bot.botkit.log("soz no emoji lulz",err);
+      }
+    });
+    var holder = "Getting transactions";
+    bot.reply(message, holder, function(){
+      controller.users.get(message.user, function(err, user){
+        mondoToken = user.mondoToken;
+        mondo.accounts(mondoToken, function(err, value){
+        if(value.accounts.length == 1){
+          var account_id = value.accounts[0].id;
+          var text = "Account: " + value.accounts[0].description + ", id: " + account_id;
+          bot.reply(message, text, function(){
+            mondo.transactions(account_id, mondoToken, function(err, value){
+              console.log(err);
+              var text = value.transactions.map(function(transaction, index){
+                return index + 1 + ". transaction: " + transaction.description + ", " + transaction.description + ", " + helpers.formatGBP(transaction.amount);
+              }).join("\n");
+              bot.reply(message, text);
+              bot.api.reactions.remove({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'thinking_face',
+                },function(err,res) {
+                if (err) {
+                  bot.botkit.log("soz i failed",err);
+                }
+              }); 
+              bot.reply(message,{
+                  text: ":smoking: simple.",
+                });
+            });
+          });
+        }
+        else {
+          bot.api.reactions.add({
+            timestamp: message.ts,
+            channel: message.channel,
+            name: 'slightly_frowning_face',
+            },function(err,res) {
+            if (err) {
+              bot.botkit.log("soz no emoji lulz",err);
+            }
+            });
+          bot.reply(message, "you have no transactions LOL go spend dat moniez $$$");
+        }
+      });
+      });    
+    });
+  });
+  wit.hears("account", 0.5, function(bot, message, outcome){
+    controller.users.get(message.user, function(err, user){
+      mondoToken = user.mondoToken;
+      if(mondoToken){
+        bot.reply(message, "These are your accounts: ");
+        mondo.accounts(mondoToken, function(err, value){
+          var text = value.accounts.map(function(account, index){
+            return index + 1 + ". Account: " + account.description;
+          }).join("\n");
+          bot.reply(message, text);
+        });
+      }
+      else{
+        bot.reply(message, {
+          text: "This would display the balance if I had a mondo token :frowning:",
+          username: "failwhale",
+          icon_url: "http://media.giphy.com/media/PIfV4QbNzslYQ/giphy.gif"
+        });
+      }
+      bot.api.reactions.remove({timestamp: message.ts, channel: message.channel, name: 'thinking_face'},function(err,res) {
+        if (err) {
+          bot.botkit.log("Failed to remove emoji reaction :(",err);
+        }
+      });
+    });
+  });
+  wit.hears("balance", 0.5, function(bot, message, outcome){
+    bot.reply(message, ":horse_racing: , retrieving...", function(){
+      controller.users.get(message.user, function(err, user){
+        mondoToken = user.mondoToken;
+        mondo.accounts(mondoToken, function(err, value){
+          if(value.accounts.length == 1){
+            var account_id = value.accounts[0].id;
+            var text = "Account: " + value.accounts[0].description;
+            bot.reply(message, text, function(){
+              mondo.balance(account_id, mondoToken, function(err, value){
+                var text = helpers.formatGBP(value.balance);
+                bot.reply(message, text);
+              });
+            });
+          }
+        });
+      });
+    });
+  });
 
   wit.otherwise(require('./lib/replies/giph.js'));
 })
